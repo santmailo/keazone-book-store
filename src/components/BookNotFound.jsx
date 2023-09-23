db9b2b92fb2f841bf85b8406d44e2fc303b89970
santmailo <iamsunil006@gmail.com>
2023-09-23 19:51:09 +0530

    search bug solved

--- src/components/BookNotFound.jsx
+++ src/components/BookNotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BookNotFound = () => {
     return (
         <div style={{backgroun:  "blue", textAlign: "center", marginTop: "20%"}}>
-            Looks like we don't have this book right now
+            Wait a Minute We are getting Books for you
         </div>
     )
 }
